6caedca7171f9cc47df62cb28672f1ef014419a3
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-10-02 20:04:26 +0100

    conditional statements

---
+++ com/learning/mahesh/NewStudentAdmissionIfElse.java
@@ -0,0 +1,34 @@
+package com.learning.mahesh;
+
+public class NewStudentAdmissionIfElse {
+    public static void main(String[] args) {
+        int classs = 55;
+        int classA = 55;
+        int classB = 55;
+        int classC = 51;
+
+        int limit = 55;
+
+
+        if (classs < limit) {
+            System.out.println("admission available in classs");
+        }else if(classA < limit) {
+            System.out.println("admissions available in classA");
+        }else if (classB < limit) {
+            System.out.println("admission available in classB");
+        }else if(classC < limit) {
+            System.out.println("admissions available in classC");
+        }else {
+            System.out.println("no spaces available for the new student");
+        }
+
+
+            }
+
+        }
+
+
+
+
+
+
---
+++ com/learning/mahesh/OperatersPractice.java
@@ -0,0 +1,11 @@
+package com.learning.mahesh;
+
+public class OperatersPractice {
+    int numberofstudentsinschool = 570;
+
+    public static void main(String[] args) {
+        int numberofmalestudents = 500;
+        int numberoffemalestudents = 70;
+
+    }
+}
