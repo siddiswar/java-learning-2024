0ba821298ac0caa88b42e17c9f1da5aa268bd36e
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-10-03 13:24:24 +0100

    new code changes

---
+++ com/learning/mahesh/Part2Operaters/OperatersBasicPractice.java
@@ -0,0 +1,17 @@
+package com.learning.mahesh.Part2Operaters;
+
+public class OperatersBasicPractice {
+    public static void main(String[] args) {
+    int A = 5;
+    int B = 10;
+    int C = 15;
+    int D = 20;
+
+        System.out.println('A');
+        System.out.println(A + B);
+        System.out.println( (A + B)%C);
+        System.out.println((A + B) + (C + D) /C);
+        System.out.println((A + B) % C / D*A - B-A);
+
+    }
+}
