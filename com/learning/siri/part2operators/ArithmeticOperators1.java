cbcaeda7544648ebda8226b4b87bb193f993a385
Sirisha Deevi <sirisha.deevi@gmail.com>
2024-10-09 17:26:36 +0100

    my additional examples

---
+++ com/learning/siri/part2operators/ArithmeticOperators1.java
@@ -0,0 +1,30 @@
+package com.learning.siri.part2operators;
+
+public class ArithmeticOperators1 {
+    public static void main(String[] args) {
+
+        int firstNumber,SecondNumber,results;
+
+        firstNumber = 30;
+        SecondNumber = 20;
+
+        int result = firstNumber + SecondNumber;
+        System.out.println(result);
+
+
+        int difference = firstNumber - SecondNumber;
+        System.out.println(difference);
+
+
+        int product = firstNumber * SecondNumber;
+        System.out.println(product);
+
+        int quotient = firstNumber / SecondNumber;
+        System.out.println(quotient);
+
+        int remainder = firstNumber % SecondNumber;
+        System.out.println(remainder);
+    }
+
+
+}
