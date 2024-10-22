a08655fe37feb5c6d0f5eff765ceeacf426dec54
sirisha Deevi <sirisha.deevi@gmail.com>
2024-10-22 12:50:31 +0100

    learn static method

---
+++ com/learning/siri/part5oops/staticmethods/MainMethodDetails.java
@@ -0,0 +1,11 @@
+package com.learning.siri.part5oops.staticmethods;
+
+public class MainMethodDetails {
+
+    public static void main(String[] args) {
+
+        for (String eachArg : args) {
+            System.out.println(eachArg);
+        }
+    }
+}
