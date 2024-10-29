49d1231b795e07dedcb9f536992134c092c44c93
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-10-29 16:18:12 +0000

    mahesh new changes

---
+++ com/learning/mahesh/part5classandobjectsbasics/staticmethods/MainMethodd.java
@@ -0,0 +1,9 @@
+package com.learning.mahesh.part5classandobjectsbasics.staticmethods;
+
+public class MainMethodd {
+
+    public static void main(String[] args) {
+        System.out.println("pl");
+
+    }
+}
