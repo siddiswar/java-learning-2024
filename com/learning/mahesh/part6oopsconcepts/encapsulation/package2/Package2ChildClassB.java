de6c7f9c89d893640bd0ddc6aa3326a5fee817ce
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-11-02 11:02:17 +0000

    encapsulation and inheritance

--- com/learning/mahesh/part6oopsconcepts/encapsulation/package2/Package2ChildClassB.java
+++ com/learning/mahesh/part6oopsconcepts/encapsulation/package2/Package2ChildClassB.java
@@ -1,4 +1,13 @@
 package com.learning.mahesh.part6oopsconcepts.encapsulation.package2;
 
-public class Package2ChildClassB {
+import com.learning.mahesh.part6oopsconcepts.encapsulation.package1.ParentClass;
+
+public class Package2ChildClassB extends ParentClass {
+    public void methodInPackage2(){
+        System.out.println(name1);
+        System.out.println(name2);
+      //  System.out.println(name3);
+      //  System.out.println(name4);
+    }
+
 }
