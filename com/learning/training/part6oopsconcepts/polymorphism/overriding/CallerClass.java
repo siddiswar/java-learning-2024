3c7e24f9069b00d81e2140de91dafabcbda8cd30
sirishadeevi0102 <116485679+sirishadeevi0102@users.noreply.github.com>
2024-11-04 12:32:46 +0000

    Polymorphism (#26)

---
+++ com/learning/training/part6oopsconcepts/polymorphism/overriding/CallerClass.java
@@ -0,0 +1,8 @@
+package com.learning.training.part6oopsconcepts.polymorphism.overriding;
+
+public class CallerClass {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeNoise();
+    }
+}
