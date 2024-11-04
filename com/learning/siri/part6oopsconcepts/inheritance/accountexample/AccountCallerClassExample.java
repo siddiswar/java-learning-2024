f4f4ad511cdb8caf31577c8f6762bf51ae7b469d
sirishadeevi0102 <116485679+sirishadeevi0102@users.noreply.github.com>
2024-11-04 10:51:06 +0000

    Siri learn inheritance and encapsulation (#25)

---
+++ com/learning/siri/part6oopsconcepts/inheritance/accountexample/AccountCallerClassExample.java
@@ -0,0 +1,9 @@
+package com.learning.siri.part6oopsconcepts.inheritance.accountexample;
+
+
+public class AccountCallerClassExample {
+    public static void main(String[] args) {
+        //Creating Object of the child class
+        BusinessAccountExample businessAccount = new BusinessAccountExample( "ACC002","Bapatla", 1234567.67, "Ananya Solutions");
+    }
+}
