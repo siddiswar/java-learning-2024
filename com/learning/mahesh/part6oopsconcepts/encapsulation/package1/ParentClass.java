f17a2e72da96254821627849214147c7f119ed46
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-11-04 10:51:44 +0000

    Mahesh part6 (#24)

---
+++ com/learning/mahesh/part6oopsconcepts/encapsulation/package1/ParentClass.java
@@ -0,0 +1,15 @@
+package com.learning.mahesh.part6oopsconcepts.encapsulation.package1;
+
+public class ParentClass {
+    public String name1;
+    protected String name2;
+    String name3;
+    private String name4;
+
+    public void methodInClassA() {
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
+    }
+}
