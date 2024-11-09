3a4eabc512d3d79742576868e43116b52ce14c87
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-11-09 12:31:34 +0000

    Mahesh part6 (#30)

---
+++ com/learning/mahesh/part6oopsconcepts/abstraction/airlinesticketbooking/AirIndia.java
@@ -0,0 +1,33 @@
+package com.learning.mahesh.part6oopsconcepts.abstraction.airlinesticketbooking;
+
+public abstract class AirIndia extends TicketBooking {
+    public double ticketPrice;
+
+    public AirIndia(int passengerage, char gender, String profession, String tickettype, double ticketPrice) {
+        super(passengerage, gender, profession, tickettype);
+        this.ticketPrice = ticketPrice;
+    }
+
+    public double calculateDiscount() {
+        double discount = 0.0;
+
+
+        if (profession.equalsIgnoreCase("Armed Forces")) {
+            discount = 0.20;
+        } else if (profession.equalsIgnoreCase("Police")) {
+            discount = 0.15;
+        }
+        return ticketPrice - (ticketPrice * discount);
+    }
+
+    public int determineTicketType() {
+        if (ticketPrice <= 200) {
+            System.out.println("Ticket type: Economy");
+        } else if (ticketPrice <= 500) {
+            System.out.println("Ticket type: Business");
+        } else {
+            System.out.println("Ticket type: First Class");
+        }
+        return 0;
+    }
+}
---
+++ com/learning/mahesh/part6oopsconcepts/abstraction/airlinesticketbooking/TicketBooking.java
@@ -0,0 +1,34 @@
+package com.learning.mahesh.part6oopsconcepts.abstraction.airlinesticketbooking;
+
+public abstract class TicketBooking {
+
+    public char gender;
+    public String profession;
+    public String tickettype;
+
+    public TicketBooking(int passengerage, char gender, String profession, String tickettype) {
+
+        this.gender = gender;
+        this.profession = profession;
+        this.tickettype = tickettype;
+
+
+    }
+
+
+    public void passengergender() {
+        if (gender == 'm') {
+            System.out.println("passenger is male");
+        } else if (gender == 'f') {
+            System.out.println("passenger gender if female");
+        } else {
+            System.out.println("gender not specified");
+        }
+
+    }
+
+    public abstract double calculateDiscount();
+
+    public abstract int determineTicketType();
+
+}
