6e12891ea44541033459ec9a2da8c2716404b2cc
mahesh052002 <118469056+mahesh052002@users.noreply.github.com>
2024-11-15 13:12:16 +0000

    changes

--- com/learning/siri/part6oopsconcepts/inheritance/accountexample/AccountCallerClassExample.java
+++
@@ -1,9 +0,0 @@
-package com.learning.siri.part6oopsconcepts.inheritance.accountexample;
-
-
-public class AccountCallerClassExample {
-    public static void main(String[] args) {
-        //Creating Object of the child class
-        BusinessAccountExample businessAccount = new BusinessAccountExample( "ACC002","Bapatla", 1234567.67, "Ananya Solutions");
-    }
-}
